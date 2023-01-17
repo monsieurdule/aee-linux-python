678d10e3d414823aafff2028778f8f8a36392a27
monsieurdule <83020438+monsieurdule@users.noreply.github.com>
2023-01-17 06:00:23 +0100

    Update test_jenkinsadmin.py

--- test_jenkinsadmin.py
+++ test_jenkinsadmin.py
@@ -1,10 +1,11 @@
-import unittest
-from api4jenkins import Jenkins
 import configparser
-import requests
-import subprocess
 import os
+import subprocess
 import time
+import unittest
+
+import requests
+from api4jenkins import Jenkins
 
 config_obj = configparser.ConfigParser()
 config_obj.read("configfile.ini")
@@ -24,42 +25,43 @@ jenkins_backup_file = JenkinsParameters["filename"]
 
 client = Jenkins(jenkins_url, auth=(jenkins_username, jenkins_password))
 
-#job = client.get_job(jenkins_job_name)
-#last_build = job.get_last_unsuccessful_build()
-#print(last_build)
-#last_unbuild = job.get_last_unsuccessful_build()
-#print(last_unbuild)
 
 class TestJenkinsadmin(unittest.TestCase):
     def test_astop(self):
-        subprocess.run(['python3', 'jenkinsadmin.py', 'stop'])
+        subprocess.run(["python3", "jenkinsadmin.py", "stop"])
         temp = requests.get(jenkins_url, auth=(jenkins_username, jenkins_password))
-        self.assertNotEqual(temp.text.find('Jenkins is going to shut down'), -1)
+        self.assertNotEqual(temp.text.find("Jenkins is going to shut down"), -1)
+
     def test_bstart(self):
-        subprocess.run(['python3', 'jenkinsadmin.py', 'start'])
+        subprocess.run(["python3", "jenkinsadmin.py", "start"])
         temp = requests.get(jenkins_url, auth=(jenkins_username, jenkins_password))
-        self.assertEqual(temp.text.find('Jenkins is going to shut down'), -1)
-    #def test_cbackup(self):
-        #subprocess.run(['python3', 'jenkinsadmin.py', 'backup']) #commented because it takes long time
-        #self.assertEqual(os.path.isfile(f'{jenkins_backup_dir}/{jenkins_backup_file}'), True)
+        self.assertEqual(temp.text.find("Jenkins is going to shut down"), -1)
+
+    def atest_cbackup(self):
+        subprocess.run(
+            ["python3", "jenkinsadmin.py", "backup"]
+        )  # commented because it takes long time
+        self.assertEqual(
+            os.path.isfile(f"{jenkins_backup_dir}/{jenkins_backup_file}"), True
+        )
+
     def test_drun(self):
-        subprocess.run(['python3', 'jenkinsadmin.py', 'run'])
+        subprocess.run(["python3", "jenkinsadmin.py", "run"])
         global job
         job = client.get_job(jenkins_job_name)
         global last_build
         last_build = job.get_last_build()
-        self.assertNotEqual(last_build, 'None')
-        #subprocess.run(['x'])
-        #print('x')
+        self.assertNotEqual(last_build, "None")
+        # subprocess.run(['x'])
+        # print('x')
+
     def test_erunstop(self):
-        time.sleep(8)
-        subprocess.run(['python3', 'jenkinsadmin.py', 'runstop'])
+        time.sleep(9)
+        subprocess.run(["python3", "jenkinsadmin.py", "runstop"])
         print(job.get_last_build())
         print(job.get_last_unsuccessful_build())
         self.assertEqual(job.get_last_unsuccessful_build(), job.get_last_build())
-        #last_build = job.get_last_unsuccessful_build()
-        #print(last_build)
 
-        
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
